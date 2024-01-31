26e1874a07d7fa7cb2f37468748afc06d417c9bf
El-Sueco <svenstauder@outlook.at>
2024-01-31 15:37:47 +0100

    init logging

--- src/main/kotlin/at/home/freezerlist/rest/HelloWorldController.kt
+++ src/main/kotlin/at/home/freezerlist/rest/HelloWorldController.kt
@@ -1,6 +1,7 @@
 package at.home.freezerlist.rest
 
 import at.home.freezerlist.rest.model.HelloWorld
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class HelloWorldController {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @GetMapping("/")
     fun getHelloWorld(): ResponseEntity<HelloWorld> {
+        log.info("getHelloWorld, getting HelloWorld")
         var helloWorld = HelloWorld();
         helloWorld.hello = "hi"
         helloWorld.world = "jupiter"
