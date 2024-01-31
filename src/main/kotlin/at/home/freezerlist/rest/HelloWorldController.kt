c9b067bf2e12cec336c55a8bb6d51b71cb01be5c
El-Sueco <svenstauder@outlook.at>
2024-01-31 15:49:20 +0100

    init post method

--- src/main/kotlin/at/home/freezerlist/rest/HelloWorldController.kt
+++ src/main/kotlin/at/home/freezerlist/rest/HelloWorldController.kt
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class HelloWorldController {
@@ -17,6 +19,14 @@ class HelloWorldController {
         var helloWorld = HelloWorld();
         helloWorld.hello = "hi"
         helloWorld.world = "jupiter"
+        return ResponseEntity.ok(helloWorld)
+    }
+
+    @PostMapping("/")
+    fun createHelloWorld(@RequestBody helloWorld: HelloWorld): ResponseEntity<HelloWorld> {
+        log.info("getHelloWorld, creating HelloWorld with {}", helloWorld)
+        helloWorld.hello = helloWorld.hello.plus(" also")
+        helloWorld.world = helloWorld.world.plus(" or somewhere else")
         return ResponseEntity.ok(helloWorld)
     }
 
