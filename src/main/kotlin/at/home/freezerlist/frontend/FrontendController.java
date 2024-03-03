ff973710453f9cc75b73d1f56bdf7bc906bf9075
El-Sueco <svenstauder@outlook.at>
2024-03-03 14:18:00 +0100

    frontend prototype

---
+++ src/main/kotlin/at/home/freezerlist/frontend/FrontendController.java
@@ -0,0 +1,18 @@
+package at.home.freezerlist.frontend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
