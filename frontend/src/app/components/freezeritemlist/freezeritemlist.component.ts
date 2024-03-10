72247c75e60abf9f3cf7ffc8d8dc5d38b1532271
El-Sueco <svenstauder@outlook.at>
2024-03-10 12:34:24 +0100

    datatables

--- frontend/src/app/components/freezeritemlist/freezeritemlist.component.ts
+++ frontend/src/app/components/freezeritemlist/freezeritemlist.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FreezeritemserviceService } from '../../service/freezeritem/freezeritemservice.service';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-freezeritemlist',
   templateUrl: './freezeritemlist.component.html',
   styleUrl: './freezeritemlist.component.css'
 })
-export class FreezeritemlistComponent {
+export class FreezeritemlistComponent implements OnInit, OnDestroy {
 
   freezeritems: any = [];
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+
   constructor(private service: FreezeritemserviceService) { }
 
   ngOnInit(): void {
     this.getFreezerItems();
-    console.log(this.freezeritems)
+    this.dtOptions = {
+      ajax: (dataTablesParameters: any, callback) => {
+        this.service
+        .getFreezerItems().subscribe(resp => {
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: resp
+            });
+          });
+      },
+      columns: [{
+        title: 'ID',
+        data: 'id'
+      }, {
+        title: 'Content',
+        data: 'content'
+      }, {
+        title: 'Freezedate',
+        data: 'freezedate'
+      }]
+    };
   }
 
   getFreezerItems() {
@@ -22,6 +46,11 @@ export class FreezeritemlistComponent {
         .getFreezerItems()
         .subscribe((response: any) => {
           this.freezeritems = response;
+          //this.dtTrigger.next();
         });
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 }
