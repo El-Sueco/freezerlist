5edd0482ff886a3cb826ce967c13ef7e1f48ada0
El-Sueco <svenstauder@outlook.at>
2024-03-13 19:01:01 +0100

    refactor event and dialog handling

--- frontend/src/app/components/freezeritemlist/freezeritemlist.component.ts
+++ frontend/src/app/components/freezeritemlist/freezeritemlist.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterRenderPhase, AfterRenderRef, AfterViewChecked, AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { FreezeritemserviceService } from '../../service/freezeritem/freezeritemservice.service';
 import { Subject } from 'rxjs';
 import { DeletefreezeritemComponent } from '../dialogs/deletedialog/deletefreezeritem/deletefreezeritem.component';
@@ -11,7 +11,7 @@ import { FreezerItem } from '../../models/freezeritem';
   selector: 'app-freezeritemlist',
   templateUrl: './freezeritemlist.component.html'
 })
-export class FreezeritemlistComponent implements OnInit, AfterViewInit {
+export class FreezeritemlistComponent implements OnInit, AfterContentChecked  {
 
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
@@ -24,6 +24,13 @@ export class FreezeritemlistComponent implements OnInit, AfterViewInit {
  
   ngOnInit(): void {
     this.loadDatatable();
+  }
+
+  ngAfterContentChecked(): void {
+    $('.addButton').off('click'); // FIXME this is indeed a workaround
+    $('.addButton').on("click", (event) => {
+      this.addButtonClick();
+    })
   }
 
   loadDatatable() {
@@ -45,25 +52,23 @@ export class FreezeritemlistComponent implements OnInit, AfterViewInit {
         title: 'Freezedate',
         data: 'freezedate',
       }, {
-        title: '<button type="button" class="btn btn-success" data-id="-1" data-operation="create">+</button>',
-        render: function (data, type, full, meta) { return '<button type="button" class="btn btn-primary"' +
-          'data-id="' + full.id + '" data-operation="getOrUpdate">/</button> <button type="button" class="btn btn-danger" data-id="' + full.id + '" data-operation="delete">#</button>'}
+        title: '<button type="button" class="addButton btn btn-success"><i class="bi bi-plus"></i></button>',
+        render: function (data, type, full, meta) { return '<button type="button" class="getOrUpdateButton btn btn-primary"' +
+          'data-id="' + full.id + '"><i class="bi bi-pen"></i></button> <button type="button" class="deleteButton btn btn-danger" data-id="' + full.id + '" data-operation="delete"><i class="bi bi-trash"></i></button>'}
       }],
       columnDefs: [
-        {targets: 2, orderable: false}
-      ]/*,
+        {targets: 2, width: "90px", orderable: false}
+      ],
       rowCallback: (row: Node, data: any[] | Object, index: number) => {
-        $('td', row).off('click');
-        $('td', row).on('click', (event) => {
-          console.log((event.target))
-          let modalRef: any;
-          modalRef = this.modalService.open(GetorupdatefreezeritemComponent);
-          modalRef.componentInstance.id = (data as FreezerItem).id
-          modalRef.result.then((result: any) => {
-            this.reloadDatatable();
-          });
+        $('.getOrUpdateButton', row).off('click');
+        $('.getOrUpdateButton', row).on('click', (event) => {
+          this.getOrUpdateButtonClick(event);
         });
-      }*/
+        $('.deleteButton', row).off('click');
+        $('.deleteButton', row).on('click', (event) => {
+          this.deleteButtonClick(event);
+        });
+      }
     };
   }
 
@@ -71,36 +76,27 @@ export class FreezeritemlistComponent implements OnInit, AfterViewInit {
     $('#freezerItemTable').DataTable().ajax.reload();
   }
 
-  ngAfterViewInit(): void {
-    this.renderer.listen('document', 'click', (event) => {
-      if (event.target.hasAttribute("data-id")) {
-        const id: number = event.target.getAttribute("data-id");
-        let modalRef: any;
-        switch (event.target.getAttribute("data-operation")){
-          case "create":
-            modalRef = this.modalService.open(CreatefreezeritemComponent);
-            modalRef.result.then((result: any) => {
-              this.reloadDatatable();
-            });
-            break;
-          case "getOrUpdate":
-            modalRef = this.modalService.open(GetorupdatefreezeritemComponent);
-            modalRef.componentInstance.id = id;
-            modalRef.result.then((result: any) => {
-              this.reloadDatatable();
-            });
-            break;
-          case "delete":
-            modalRef = this.modalService.open(DeletefreezeritemComponent);
-            modalRef.componentInstance.id = id;
-            modalRef.result.then((result: any) => {
-              this.reloadDatatable();
-            });
-            break;
-          default:
-            throw new Error("operation not supported!")
-        }
-      }
+  addButtonClick() {
+    let modalRef: any;
+    modalRef = this.modalService.open(CreatefreezeritemComponent);
+    modalRef.result.then((result: any) => {
+      this.reloadDatatable();
+    });
+  }
+
+  getOrUpdateButtonClick(event: any) {
+    let modalRef: any = this.modalService.open(GetorupdatefreezeritemComponent);
+    modalRef.componentInstance.id = event.currentTarget.dataset["id"];
+    modalRef.result.then((result: any) => {
+      this.reloadDatatable();
+    });
+  }
+
+  deleteButtonClick(event: any) {
+    let modalRef: any = this.modalService.open(DeletefreezeritemComponent);
+    modalRef.componentInstance.id = event.currentTarget.dataset["id"];
+    modalRef.result.then((result: any) => {
+      this.reloadDatatable();
     });
   }
 }
